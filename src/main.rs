ab1ef124e2e84d5fe18332dec1da2d522d92d8fc
Duncan Sung W. Kim <DuncanSungWKim@gmail.com>
2023-11-12 22:51:17 +0900

    List image file paths in the input folder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{
+    path::Path, fs, io::prelude::*
+};
+
+
+
+static CURRENT_DIR : &str = "." ;
+static INPUT_DIR   : &str = "input" ;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let inputDir = Path::new( CURRENT_DIR ).join( INPUT_DIR ) ;
+    let paths = fs::read_dir( inputDir ).unwrap() ;
+    for path in paths {
+    println!("Hello, {}!", path.unwrap().path().display() );
+    }
 }
