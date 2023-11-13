5c4fa19c2e1d9810e2864862d2fa1d7963a6dd26
Duncan Sung W. Kim <DuncanSungWKim@gmail.com>
2023-11-13 16:19:59 +0900

    Prepare the output folder

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,28 @@ use std::{
 
 static CURRENT_DIR : &str = "." ;
 static INPUT_DIR   : &str = "input" ;
+static OUTPUT_DIR  : &str = "output" ;
 
 
 
 fn main() {
     let inputDir = Path::new( CURRENT_DIR ).join( INPUT_DIR ) ;
+    let outputDir = PrepareOutputDir() ;
     let paths = fs::read_dir( inputDir ).unwrap() ;
     for path in paths {
     println!("Hello, {}!", path.unwrap().path().display() );
     }
+}
+
+
+
+fn PrepareOutputDir(
+)-> Box<Path>
+{
+    let outputDir = Path::new( CURRENT_DIR ).join( OUTPUT_DIR ) ;
+    if outputDir.is_dir() {
+        fs::remove_dir_all( &outputDir ) ;
+    }
+    fs::create_dir( &outputDir ) ;
+    outputDir.into_boxed_path()
 }
